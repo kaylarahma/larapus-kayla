2eaa22902f8989b0bfd394eaf7734e6cd0f4a8f5
kaylarahma <kaylarahmanisa83@gmail.com>
2021-10-22 22:19:42 +0700

    Larapuss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Laravel Perpustakaan
+                </div>
+
+                <div class="card-body">
+                    Selamat Datang di <strong>Larapus</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
