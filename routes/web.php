440c4dba964744b148f4729487c6d2fca2b6d1de
kaylarahma <kaylarahmanisa83@gmail.com>
2021-11-11 11:37:53 +0700

    crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,5 +31,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
     Route::get('/', function () {
         return view('admin.index');
     });
-
+    Route::resource('author', AuthorController::class);
+    Route::resource('books', BookController::class);
 });
